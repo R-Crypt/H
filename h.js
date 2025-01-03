acef849523ac608632ad86b496f167654b237630
R-Crypt <68041569+R-Crypt@users.noreply.github.com>
2025-01-03 21:57:25 +0530

    Update h.js

--- h.js
+++ h.js
@@ -1,5 +1,5 @@
 document.getElementById('apologyButton').addEventListener('click', () => {
-    const message = document.getElementById('message');
-    message.classList.remove('hidden');
-    message.classList.add('visible');
-  });
+  const message = document.getElementById('message');
+  message.classList.remove('hidden');
+  message.classList.add('visible');
+});
